7bcd4864795ec2d48d35bb3f88d11cf00bed6bfd
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-10 13:31:29 +0530

    Add files via upload

---
+++ program 31.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a=4;
+    int b=5;
+    printf("%d\n",a*b);
+    int c=10;
+    int d=2;
+    printf("%d\n",c*d);
+    int e=1;
+    int f=20;
+    printf("%d\n",e*f);
+    return 0;
+}
