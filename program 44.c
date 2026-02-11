6587fbaaddbecfd215e5559f82df53ccc81f55da
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-11 13:31:21 +0530

    Add files via upload

---
+++ program 44.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a=5, b=1, c=0;
+    int d=3, e=1, f=5;
+    printf("%d\n",a&&d,a||d); 
+    printf("%d\n",b&&e,b||e);
+    printf("%d\n",c&&f,c||f);
+    
+    
+    
+    return 0;
+}
