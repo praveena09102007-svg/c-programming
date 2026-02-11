d420405f6e42016152390d078ef773f8c56acec0
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-11 09:47:30 +0530

    Add files via upload

---
+++ program 32.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a=9, b=2;
+    int c=10, d=3;
+    int e=20, f=4;
+    printf("Quotient=%d Remainder=%d",a/b,a%b);
+    printf("\nQuotient=%d Remainder=%d",c/d,c%d);
+    printf("\nQuotient=%d Remainder=%d",e/f,e%f);
+    return 0;
+}
