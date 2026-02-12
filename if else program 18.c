404ac27c85485f73991f641bb154f6c10ae3fd2f
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-12 15:49:21 +0530

    Add files via upload

---
+++ if else program 18.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=0&&a<=100){
+    printf("bill amount rs.600");
+    }
+    else if(a>=101&&a<=600){
+    printf("bill amount rs.600");
+    }
+    return 0;
+}
