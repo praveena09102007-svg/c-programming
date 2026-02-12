101228581bcadb7705a3d409146e786b45ea860f
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-12 13:44:45 +0530

    Add files via upload

---
+++ if else program 11.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a<=b){
+    printf("25 is largest");
+    }
+    else if(a<=c){
+        
+    }
+    else 
+    printf("25 is not largest");
+    return 0;
+}
