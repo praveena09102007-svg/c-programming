872df56f80f2eb0378d05c804a91d0b2bc4a6c3f
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-12 14:28:30 +0530

    Add files via upload

---
+++ if else program 17.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=10&&a<=20){
+    printf("number is within range");
+    }
+    else{
+    printf("not number is within in range");
+    }
+    return 0;
+}
