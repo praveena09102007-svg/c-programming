02feb23a79b68dc7c2321b9d119cc680eab9696c
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-12 13:53:42 +0530

    Add files via upload

---
+++ if else program 12.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a,&b);
+    if(a>=75,b<90){
+    printf("grade b");
+    }
+    else{ 
+    printf("grade not b");
+    }
+    return 0;
+}
