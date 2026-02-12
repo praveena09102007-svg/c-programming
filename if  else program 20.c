cfcff4b1f38ffa101f328e64b47e269d599b7530
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-12 15:58:49 +0530

    Add files via upload

---
+++ if  else program 20.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a>=b,b<=a){
+    printf("3 is smallest");
+    }
+    else {
+    printf("3 is not smallest");
+    }
+    return 0;
+}
