ce61a8b3ec347bacc71358ade72addb1064f250f
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-12 11:12:10 +0530

    Add files via upload

---
+++ if else program 3.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%5==0)
+    printf("divisible 5");
+    else{
+        printf("not divisible 5");
+    }
+
+    return 0;
+}
