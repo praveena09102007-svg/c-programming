25444f922f34ffe431647edb1fbe87441cb1f627
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-12 13:59:38 +0530

    Add files via upload

---
+++ if else program 13.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=65&&a>=97){
+    printf("upper case letter");
+    }
+    else{ 
+    printf("not upper case letter");
+    }
+    return 0;
+}
