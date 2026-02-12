71c45e01e35f18e7f1d831db27a40a1cf283cdc2
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-12 11:13:41 +0530

    Add files via upload

---
+++ if else program 4.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a ,&b);
+    if(a<b)
+    printf("greater then 20");
+    else{
+        printf("greater then not 20");
+    }
+
+    return 0;
+}
