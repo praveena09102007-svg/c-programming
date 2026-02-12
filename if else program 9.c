2a3ba945a5e839b819567378473e4786e58214b5
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-12 12:13:51 +0530

    Add files via upload

---
+++ if else program 9.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if("age>=18"){
+    printf("eligble to vote");
+    }
+    else 
+    printf("not eligble to vote");
+    return 0;
+}
