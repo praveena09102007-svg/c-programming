c786bc55b556e4030ab1885d068a8b763c3972fe
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-13 10:59:09 +0530

    Add files via upload

---
+++ if else program 22.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int day;
+    scanf("%d",&day);
+    switch(day){
+        case 1:
+        printf("monday");
+        break;
+        case 2:
+        printf("tuesday");
+        break;
+        case 3:
+        printf("wednesday");
+        break;
+        case 4:
+        printf("thursday");
+        break;
+        case 5:
+        printf("friday");
+        break;
+        case 6:
+        printf("saturday");
+        break;
+        printf("sunday");
+    default:
+    
+
+    printf("enter valid day");
+    }
+
+    return 0;
+}
