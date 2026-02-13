fbe98f3592b31095410b78e16f77c366b3b8d092
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-13 12:24:52 +0530

    Add files via upload

---
+++ if else program 25.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int choice;
+    int a,b;
+    
+    scanf("%d",&choice);
+    switch(choice){
+    case 1:
+    scanf("%d %d", &a,&b);
+    printf("sum = %d",a+b);
+    break;
+    default:
+    printf("invalid choice");
+    }
+
+    return 0;
+}
