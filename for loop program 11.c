a8d80048bf71c4b9b0969cbff0c893e1f93a010c
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-14 16:16:50 +0530

    Add files via upload

---
+++ for loop program 11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int sum;
+    scanf("%d",&n);
+    for(int i=1;i<=3;i++){
+        sum=sum+i;
+    }
+    
+    printf("%d\n",sum);
+
+    return 0;
+}
+
