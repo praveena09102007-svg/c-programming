cb8c935d841a7518a4d2ba48453dea2f4dbe54ec
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-14 09:45:03 +0530

    Add files via upload

---
+++ for loop program 4.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    for(int i=5;i<=50;i+=5){
+    printf("%d\n",i);
+    }
+
+
+    return 0;
+}
