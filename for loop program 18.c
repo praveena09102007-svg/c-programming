73eb350006e9d8ce322b03b482a01740aa656037
praveena09102007-svg <praveena09102007@gmail.com>
2026-02-17 11:05:39 +0530

    Add files via upload

---
+++ for loop program 18.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+
+int main()
+{  
+    int a,b,i,gcd;
+    scanf("%d %d",&a,&b);
+    for(int i=1;i<=a && i<b;i++){
+        if(a%i == 0 && b%i == 0){
+           gcd=i;
+        }
+    }
+    printf("the gcd of %d and %d  is %d\n",a,b,gcd);
+
+    return 0;
+}
